b11a86fbc223dd5a72614185e34970a2413d128b
allthetok <al.tokjuman@gmail.com>
2024-02-27 19:37:04 -0500

    finish timeline component

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,27 +2,23 @@
 import React from 'react'
 import { useActiveSectionContext } from '@/context/action-section-context'
 import { useSectionInView } from '@/hooks/sectionscroll'
-import { WorkTimeItem } from './WorkTimeItem'
-import { timelineData } from '@/content/textdata'
-import { TimeLineDtl } from '@/helpers/types'
 
-const WorkTime = () => {
+const About = () => {
 	const { ref } = useSectionInView('Home', 0.5)
 	const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext()
+
 	return (
-		<section ref={ref} id='home' className='mt-[90rem] mb-28 max-w-[60rem] text-center sm:mb-0'>
+		<section ref={ref} id='about' className='mt-[90rem] mb-28 max-w-[60rem] text-center sm:mb-0'>
 			<div className='flex flex-col'>
 				<h2 className='text-slate-300 text-7xl font-extrabold'>Work Experience</h2>
-				<div className='flex flex-col md:flex-row justify-center my-20'>
+				{/* <div className='flex flex-col md:flex-row justify-center my-20'>
 					<div className='w-full md:w-7/12'>
 						{timelineData.map((item: TimeLineDtl, index: number) => (
 							<WorkTimeItem timeItem={item} key={index} />
 						))}
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</section>
 	)
 }
-
-export { WorkTime }
--- src/Components/WorkTimeItem.tsx
+++ src/Components/WorkTimeItem.tsx
@@ -10,8 +10,16 @@ const WorkTimeItem = ({ timeItem }: WorkTimeItemProps) => {
 		<ol className='flex flex-col md-flex-row relative border-l border-stone-200'>
 			<li className='mb-10 ml-4'>
 				<div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-[1.5rem] -left-1.5 border border-white' />
-				<p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-					<span className='inline-block px-2 py-1 font-semibold text-white bg-stone-900'
+				<div className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
+					<span className='inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md'>
+						{timeItem.from} - {timeItem.to}
+					</span>
+					<div className='text-xl font-semibold text-gray-300'>
+						{timeItem.title} - {timeItem.company}
+					</div>
+				</div>
+				<p className='my-2 text-base font-normal text-white'>
+					{timeItem.details}
 				</p>
 			</li>
 		</ol>
