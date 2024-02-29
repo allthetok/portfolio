b72e5bcdc1e8ec196becee1f6aed1301a6d6db24
allthetok <al.tokjuman@gmail.com>
2024-02-29 16:21:08 -0500

    update folder of assets

--- src/content/textdata.ts
+++ src/content/textdata.ts
@@ -64,29 +64,29 @@ const projects: ProjectDtl[] = [
 	{
 		name: 'GamesAtlas',
 		description: "I'm a passionate software engineer with over 5 years of experience in the field. I thrive on turning ideas into reality through elegant and efficient code. My journey in software development has led me through various projects, each one teaching me new skills and pushing the boundaries of what's possible. In my 5 years in the industry, I've honed my skills in [mention specific technologies or areas you specialize in, e.g., full-stack development, front-end frameworks like Angular, back-end with Node.js, etc.]. I've had the pleasure of working on diverse projects, from developing scalable web applications to optimizing database performance. Each project has been a learning experience, refining my problem-solving abilities and attention to detail.",
-		images: ['/GA1.JPG', '/GA2.JPG', '/GA3.JPG', '/GA4.JPG', '/GA5.JPG', '/GA6.JPG', '/GA7.JPG', '/GA8.JPG', '/GA9.JPG', '/GA10.JPG', '/GA11.JPG', '/GA12.JPG', '/GA13.JPG', '/GA14.JPG', '/GA15.JPG', '/GA16.JPG', '/GA17.JPG', '/GA19.JPG', '/GA20.JPG', '/GA21.JPG', '/GA22.JPG', '/GA23.JPG'],
+		images: ['GamesAtlas/GA1.JPG', 'GamesAtlas/GA2.JPG', 'GamesAtlas/GA3.JPG', 'GamesAtlas/GA4.JPG', 'GamesAtlas/GA5.JPG', 'GamesAtlas/GA6.JPG', 'GamesAtlas/GA7.JPG', 'GamesAtlas/GA8.JPG', 'GamesAtlas/GA9.JPG', 'GamesAtlas/GA10.JPG', 'GamesAtlas/GA11.JPG', 'GamesAtlas/GA12.JPG', 'GamesAtlas/GA13.JPG', 'GamesAtlas/GA14.JPG', 'GamesAtlas/GA15.JPG', 'GamesAtlas/GA16.JPG', 'GamesAtlas/GA17.JPG', 'GamesAtlas/GA19.JPG', 'GamesAtlas/GA20.JPG', 'GamesAtlas/GA21.JPG', 'GamesAtlas/GA22.JPG', 'GamesAtlas/GA23.JPG'],
 		libraries: ['HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React'],
 		link: 'https://at.games-atlas.xyz/',
 		frontend: 'https://github.com/allthetok/Gamesatlasfe',
 		backend: 'https://github.com/allthetok/GamesAtlas'
 	},
 	{
-		name: 'GamesAtlas',
+		name: 'AT Kicks',
 		description: "I'm a passionate software engineer with over 5 years of experience in the field. I thrive on turning ideas into reality through elegant and efficient code. My journey in software development has led me through various projects, each one teaching me new skills and pushing the boundaries of what's possible. In my 5 years in the industry, I've honed my skills in [mention specific technologies or areas you specialize in, e.g., full-stack development, front-end frameworks like Angular, back-end with Node.js, etc.]. I've had the pleasure of working on diverse projects, from developing scalable web applications to optimizing database performance. Each project has been a learning experience, refining my problem-solving abilities and attention to detail.",
-		images: ['/GA1.JPG', '/GA2.JPG', '/GA3.JPG', '/GA4.JPG', '/GA5.JPG', '/GA6.JPG', '/GA7.JPG', '/GA8.JPG', '/GA9.JPG', '/GA10.JPG', '/GA11.JPG', '/GA12.JPG', '/GA13.JPG', '/GA14.JPG', '/GA15.JPG', '/GA16.JPG', '/GA17.JPG', '/GA19.JPG', '/GA20.JPG', '/GA21.JPG', '/GA22.JPG', '/GA23.JPG'],
+		images: ['ATKicks/AK1.JPG', 'ATKicks/AK2.JPG', 'ATKicks/AK3.JPG', 'ATKicks/AK4.JPG', 'ATKicks/AK5.JPG', 'ATKicks/AK6.JPG', 'ATKicks/AK7.JPG', 'ATKicks/AK8.JPG', 'ATKicks/AK9.JPG', 'ATKicks/AK10.JPG', 'ATKicks/AK11.JPG', 'ATKicks/AK12.JPG', 'ATKicks/AK13.JPG', 'ATKicks/AK14.JPG', 'ATKicks/AK16.JPG', 'ATKicks/AK17.JPG', 'ATKicks/AK18.JPG', 'ATKicks/AK19.JPG'],
 		libraries: ['HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React'],
-		link: 'https://at.games-atlas.xyz/',
-		frontend: 'https://github.com/allthetok/Gamesatlasfe',
-		backend: 'https://github.com/allthetok/GamesAtlas'
+		link: 'https://atkicks.allentok.com/',
+		frontend: 'https://github.com/allthetok/FrontEndEC',
+		backend: 'https://github.com/allthetok/Ecommerce'
 	},
 	{
-		name: 'GamesAtlas',
+		name: 'PodFinder',
 		description: "I'm a passionate software engineer with over 5 years of experience in the field. I thrive on turning ideas into reality through elegant and efficient code. My journey in software development has led me through various projects, each one teaching me new skills and pushing the boundaries of what's possible. In my 5 years in the industry, I've honed my skills in [mention specific technologies or areas you specialize in, e.g., full-stack development, front-end frameworks like Angular, back-end with Node.js, etc.]. I've had the pleasure of working on diverse projects, from developing scalable web applications to optimizing database performance. Each project has been a learning experience, refining my problem-solving abilities and attention to detail.",
-		images: ['/GA1.JPG', '/GA2.JPG', '/GA3.JPG', '/GA4.JPG', '/GA5.JPG', '/GA6.JPG', '/GA7.JPG', '/GA8.JPG', '/GA9.JPG', '/GA10.JPG', '/GA11.JPG', '/GA12.JPG', '/GA13.JPG', '/GA14.JPG', '/GA15.JPG', '/GA16.JPG', '/GA17.JPG', '/GA19.JPG', '/GA20.JPG', '/GA21.JPG', '/GA22.JPG', '/GA23.JPG'],
+		images: ['PodFinder/PF1.JPG', 'PodFinder/PF2.JPG', 'PodFinder/PF3.JPG', 'PodFinder/PF4.JPG', 'PodFinder/PF5.JPG', 'PodFinder/PF6.JPG', 'PodFinder/PF7.JPG', 'PodFinder/PF8.JPG', 'PodFinder/PF9.JPG', 'PodFinder/PF10.JPG', 'PodFinder/PF11.JPG', 'PodFinder/PF12.JPG', 'PodFinder/PF13.JPG'],
 		libraries: ['HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React', 'NextJS', 'HTML', 'CSS', 'React'],
-		link: 'https://at.games-atlas.xyz/',
-		frontend: 'https://github.com/allthetok/Gamesatlasfe',
-		backend: 'https://github.com/allthetok/GamesAtlas'
+		link: 'https://github.com/allthetok/PodApp',
+		frontend: 'https://github.com/allthetok/PodApp',
+		backend: 'https://github.com/allthetok/PodApp'
 	}
 ]
 
