2886b2eac4119f8a19822daba3ac74a0e8e4660c
allthetok <al.tokjuman@gmail.com>
2024-03-11 20:31:55 -0400

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./src/components/**/*.{html,js,tsx}',
+		'./src/Components/**/*.{html,js,tsx}',
 		'./app/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
